c51960559cb3353cc8475614c29d2ccf4d637166
YouROK <8YouROK8@mail.ru>
2016-07-25 16:42:58 +0300

    Add app for notify

---
+++ src/gui/app.go
@@ -0,0 +1,19 @@
+package gui
+
+import (
+	"github.com/gotk3/gotk3/glib"
+)
+
+var (
+	app *glib.Application
+)
+
+func InitApp() {
+	app = glib.ApplicationNew("ru.YouROK.SRadio", glib.APPLICATION_FLAGS_NONE)
+
+}
+
+func RunApp(f func()) {
+	app.Connect("activate", f)
+	app.Run(nil)
+}
